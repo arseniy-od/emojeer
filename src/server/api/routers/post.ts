91b72e9b8a65e6997cd0f32630ec8bb4fd62799c
arseniy <odarseniy@gmail.com>
2023-10-23 21:49:50 +0300

    Added getAll post route

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -3,14 +3,6 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
   create: publicProcedure
     .input(z.object({ content: z.string().min(1),
     authorId: z.string().min(1) }))
